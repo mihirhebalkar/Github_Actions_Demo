2e513e8a5a01bf2279ae65f15b9c953808df0a2b
mihirhebalkar <mihirhebalkar07@gmail.com>
2025-09-13 14:33:05 +0530

    Add new code

---
+++ app.py
@@ -0,0 +1,6 @@
+def add(a,b):
+    return a+b
+
+if __name__ == "__main__":
+    ans = add(5,3)
+    print("3 + 5 = ",ans)
---
+++ test_app.py
@@ -0,0 +1,5 @@
+from app import add
+
+
+def test_add():
+    assert add(3,5)
